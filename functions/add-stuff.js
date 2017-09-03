19375cb0f9ad9e764f4c4e44c1343a87557c1616
Laurie Boyes <laurie.boyes@ft.com>
2017-09-03 18:23:41 +0100

    post function

---
+++ functions/add-stuff.js
@@ -0,0 +1,31 @@
+
+const AWS = require('aws-sdk');
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
+module.exports.handle = (event, context, callback) => {
+
+	const tableName = event.pathParameters.tableName;
+	const date = new Date();
+
+	console.log('Adding new event to table ' + tableName);
+
+	documentClient.put({
+		TableName: tableName,
+		Item: {
+			dateTime: date.getTime(),
+			isoDateTime: date.toISOString()
+		}
+	}, (err) => {
+		if (err) {
+			callback(err);
+		} else {
+			const response = {
+				statusCode: 200,
+				body: JSON.stringify({
+					message: 'success'
+				}),
+			};
+			callback(null, response);
+		}
+	})
+};
